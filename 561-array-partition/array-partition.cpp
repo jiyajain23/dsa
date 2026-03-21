6569f69cff496a6646007fbd1953a6cdd07413f4
Jiya Jain <jiyajain2312@gmail.com>
2026-03-21 15:10:33 +0530

    Time: 11 ms (54.26%) | Memory: 32 MB (84.17%) - LeetSync

---
+++ 561-array-partition/array-partition.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int ans=0;
+        int i=0;
+        while(i<nums.size()){
+            ans+=nums[i];
+            i+=2;
+        }
+        return ans;
+    }
+};
