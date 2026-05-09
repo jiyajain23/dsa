9e1b4cd7973dd21ec7677bf017be6aa1168d0e53
Jiya Jain <jiyajain2312@gmail.com>
2026-05-09 17:52:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Spanning Tree - Kruskal's Algorithm/minimum-spanning-tree-kruskals-algorithm.cpp
@@ -0,0 +1,58 @@
+// User function Template for C++
+class DisjointSet {
+    vector<int> rank, parent;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+};
+class Solution {
+  public:
+    static bool compare(vector<int> &a,vector<int>&b){
+        return a[2]<b[2];
+    }
+    int kruskalsMST(int V, vector<vector<int>> &edges) {
+        // code here
+        sort(edges.begin(),edges.end(),compare);
+        int mst=0;
+        DisjointSet ds(V);
+        
+        for(auto it:edges){
+            int u=it[0];
+            int v=it[1];
+            int w=it[2];
+            if(ds.findUPar(u)!=ds.findUPar(v)){
+                mst+=w;
+                ds.unionByRank(u,v);
+            }
+        }
+        return mst;
+        
+    }
+};
