afb10d10fe9aaf2c69620d632bb2312f74b74ec7
Jiya Jain <jiyajain2312@gmail.com>
2026-06-16 22:16:04 +0530

    Time: 8 ms (26.89%) | Memory: 39.9 MB (42.05%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     string processStr(string s) {
         string ans = "";
-
         for (int i = 0; i < s.size(); i++) {
             if (s[i] == '#') {
                 ans += ans;
             } else if (s[i] == '*') {
-                if (!ans.empty())
-                    ans.pop_back();
-                else
-                    continue;
+                if (!ans.empty()) ans.pop_back();
+                else continue;
             } else if (s[i] == '%') {
                 reverse(ans.begin(), ans.end());
             } else {
